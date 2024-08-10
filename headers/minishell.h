33e260deb299f10aa278a43c061bf9e329857415
adamkhobba <adamekhobba6@gmail.com>
2024-08-10 18:12:43 +0100

    Update .gitignore and minishell.h, and remove garbage code

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: akhobba <akhobba@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 01:00:00 by oouaadic          #+#    #+#             */
-/*   Updated: 2024/08/10 15:47:30 by akhobba          ###   ########.fr       */
+/*   Updated: 2024/08/10 16:16:23 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,17 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <signal.h>
-# include <stropts.h>
 # include <termios.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+typedef struct s_tree
+{
+    t_type				type;
+    char				*content;
+    int                 status_par;
+    struct s_tree		*left;
+    struct s_tree		*right;
+}               t_tree;
 
 #endif //MINISHELL_H
