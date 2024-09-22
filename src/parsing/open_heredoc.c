4a77b1e57bc2e8cc34a38648de29e4e3c502f635
adamkhobba <adamekhobba6@gmail.com>
2024-09-22 23:52:13 +0100

    heredoc fixed

--- src/parsing/open_heredoc.c
+++ src/parsing/open_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: akhobba <akhobba@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 18:58:46 by akhobba           #+#    #+#             */
-/*   Updated: 2024/09/19 11:10:44 by akhobba          ###   ########.fr       */
+/*   Updated: 2024/09/22 23:49:33 by akhobba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,55 +44,57 @@ char	*ft_write_heredoc(char *limit, int num, bool key_expand)
 	char	*line[2];
 	int		line_num;
 	int		fd;
+	pid_t	pid;
 
+	pid = fork();
 	line[0] = ft_name_file(num);
 	fd = ft_get_fd(line[0]);
 	line_num = 0;
-	while (true)
+	if (pid == 0)
 	{
-		line[1] = readline("> ");
-		if (line[1] && *line[1] && !isempty(line[1]))
-			add_history(line[1]);
-		if (++line_num && !line[1])
+		signal(SIGINT, SIG_DFL);
+		while (true)
 		{
-			ft_error_msg(limit, line_num);
-			break ;
+			line[1] = readline("> ");
+			if (line[1] && *line[1] && !isempty(line[1]))
+				add_history(line[1]);
+			if (++line_num && !line[1])
+			{
+				ft_error_msg(limit, line_num);
+				break ;
+			}
+			if (*line[1] && ft_strncmp(line[1], limit, ft_strlen(line[1])) == 0)
+			{
+				free(line[1]);
+				break ;
+			}
+			ft_write_n(&line[1], fd, key_expand);
 		}
-		if (*line[1] && ft_strncmp(line[1], limit, ft_strlen(line[1])) == 0)
-		{
-			free(line[1]);
-			break ;
-		}
-		ft_write_n(&line[1], fd, key_expand);
+		return (free(limit), close(fd), ft_exit(EXIT_SUCCESS),line[0]);
 	}
+	waitpid(pid, NULL, 0);
 	return (free(limit), close(fd), line[0]);
 }
 
 int	ft_open_herdoc(t_link **link, bool key_expand, int num)
 {
-	pid_t	pid;
+	t_link	*tmp;
 
-	pid = fork();
-	if (pid == 0)
+	tmp = *link;
+	while (tmp)
 	{
-		signal(SIGINT, SIG_DFL);
-		while (*link)
+		if ((tmp)->identifier == HEREDOC
+			&& ((tmp)->next && (tmp)->next->identifier == STR))
 		{
-			if ((*link)->identifier == HEREDOC
-				&& ((*link)->next && (*link)->next->identifier == STR))
-			{
-				key_expand = is_inquotes((*link)->next->command, 1);
-				(*link)->next->command
-					= ft_tmp_rmquotes((*link)->next->command);
-				(*link)->next->command
-					= ft_write_heredoc((*link)->next->command,
-						num++, key_expand);
-			}
-			*link = (*link)->next;
+			key_expand = is_inquotes((tmp)->next->command, 1);
+			(tmp)->next->command
+				= ft_tmp_rmquotes((tmp->next->command));
+			(tmp)->next->command
+				= ft_write_heredoc((tmp)->next->command,
+					num++, key_expand);
 		}
-		ft_exit(EXIT_SUCCESS);
+		tmp = (tmp)->next;
 	}
 	signal(SIGINT, SIG_IGN);
-	waitpid(pid, NULL, 0);
 	return (NONE);
 }
